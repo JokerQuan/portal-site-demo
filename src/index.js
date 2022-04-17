1862ea8ca050f863750a4aa52018c5a611df15b8
JokerQuan <jokeshuan@gmail.com>
2022-04-17 16:43:01 +0800

    路由修改为hashrouter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import SamplePage from './page/SamplePage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/sample" element={<SamplePage />} />
         <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
